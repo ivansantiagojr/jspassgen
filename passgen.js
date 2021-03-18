d89c11bd96f2bc075a960e3ab2f821c295535826
ivan <ivansantiago.junior@gmail.com>
2021-03-17 23:44:44 -0300

    weak passwords

---
+++ passgen.js
@@ -0,0 +1,28 @@
+function password(length, num = false, strength = 'weak'){
+	var lower = "abcdefghijklnmopqrstuvwxyz"
+	var upper = lower.toUpperCase()
+	var letters = lower + upper
+	var digits = "0123456789"
+	var punctuation = "{}[]´`~^:;.,/?+=-!@#$%&*()"
+	var pwd = ""
+
+	if(strength == 'weak'){
+		if(num){
+			length -= 2
+		
+			for(let i = 0; i <= 2; i++){
+				pwd += digits.charAt(Math.floor(Math.random() * digits.length))
+			}
+			for(let j = 0; j <= length; j++){
+				pwd += lower.charAt(Math.floor(Math.random() * lower.lenght))
+			}
+		}
+	 }// else if(strength == 'strong'){
+	// 	if(num){
+	// 		length -=2
+	// 		for(let k
+	// }
+
+}
+
+console.log(password(5, num = true, strength = 'weak'))
