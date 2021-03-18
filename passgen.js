ba9b367ee64e5f621cac44edb61e27bcc5469678
ivan <ivansantiago.junior@gmail.com>
2021-03-18 10:13:58 -0300

    added shuffle function

--- passgen.js
+++ passgen.js
@@ -1,35 +1,48 @@
-function password(length, num = false, strength = 'weak'){
-	var lower = "abcdefghijklnmopqrstuvwxyz"
-	var upper = lower.toUpperCase()
-	var letters = lower + upper
-	var digits = "0123456789"
-	var specialChars = "{}[]´`~^:;.,/?+=-!@#$%&*()"
-	var pwd = ""
+function password(length, num = false, strength = "weak") {
+  var lower = "abcdefghijklnmopqrstuvwxyz";
+  var upper = lower.toUpperCase();
+  var letters = lower + upper;
+  var digits = "0123456789";
+  var specialChars = "{}[]´`~^:;.,/?+=-!@#$%&*()";
+  var pwd = "";
 
-	if(strength == 'weak'){
-		if(num){
-			length -= 2
-		
-			for(let i = 0; i <= 2; i++){
-				pwd += digits.charAt(Math.floor(Math.random() * digits.length))
-			}
-			for(let j = 0; j <= length; j++){
-				pwd += lower.charAt(Math.floor(Math.random() * lower.length))
-			}
+  if (strength == "weak") {
+    if (num) {
+      length -= 2;
+
+      for (let i = 0; i <= 2; i++) {
+        pwd += digits.charAt(Math.floor(Math.random() * digits.length));
+      }
+      for (let j = 0; j <= length; j++) {
+        pwd += lower.charAt(Math.floor(Math.random() * lower.length));
+      }
+    }
+  } else if (strength == "strong") {
+    if (num) {
+      length -= 2;
+      for (let k = 0; k <= 2; k++) {
+        pwd += digits.charAt(Math.floor(Math.random() * digits.length));
+      }
+
+      for (let j = 0; j <= length; j++) {
+        pwd += letters.charAt(Math.floor(Math.random() * letters.length));
+      }
+    }
+
+    pwd = pwd.split("")
+	const shuffleArray = array => {
+		for(let i = array.length; i > 0; i--){
+			const j = Math.floor(Math.random() * (i + 1))
+			const temp = array[i]
+			array[i] = array[j];
+			array[j] = temp
 		}
-	 } else if(strength == 'strong'){
-		if(num){
-			length -=2
-			for(let k = 0; k <= 2; k++){
-				pwd += digits.charAt(Math.floor(Math.random() * digits.length))
-			}
-
-			for(let j = 0; j <= length; j++){
-				pwd += letters.charAt(Math.floor(Math.random() * letters.length))
-			}
-
+		return array
 	}
-return pwd
-}}
+	shuffleArray(pwd)
+	pwd = pwd.join("")
+    return pwd;
+  }
+}
 
-console.log(password(8, num = true, strength = 'strong'))
+console.log(password(8, (num = true), (strength = "strong")));
