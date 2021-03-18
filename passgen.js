bed8d0e5ea3096d24c06b6f25761e30165dd2205
ivan <ivansantiago.junior@gmail.com>
2021-03-18 09:52:01 -0300

    added strong passwords

--- passgen.js
+++ passgen.js
@@ -3,7 +3,7 @@ function password(length, num = false, strength = 'weak'){
 	var upper = lower.toUpperCase()
 	var letters = lower + upper
 	var digits = "0123456789"
-	var punctuation = "{}[]´`~^:;.,/?+=-!@#$%&*()"
+	var specialChars = "{}[]´`~^:;.,/?+=-!@#$%&*()"
 	var pwd = ""
 
 	if(strength == 'weak'){
@@ -17,12 +17,19 @@ function password(length, num = false, strength = 'weak'){
 				pwd += lower.charAt(Math.floor(Math.random() * lower.length))
 			}
 		}
-	 }// else if(strength == 'strong'){
-	// 	if(num){
-	// 		length -=2
-	// 		for(let k
-	// }
-return pwd
-}
+	 } else if(strength == 'strong'){
+		if(num){
+			length -=2
+			for(let k = 0; k <= 2; k++){
+				pwd += digits.charAt(Math.floor(Math.random() * digits.length))
+			}
 
-console.log(password(5, num = true, strength = 'weak'))
+			for(let j = 0; j <= length; j++){
+				pwd += letters.charAt(Math.floor(Math.random() * letters.length))
+			}
+
+	}
+return pwd
+}}
+
+console.log(password(8, num = true, strength = 'strong'))
